aa1dc5061f9bd28623141c1ce9ea7faa0fecd1f6
Chen Wenjia <Wenjia_Chen@StateStreet.com>
2012-12-14 15:49:56 +0800

    clean

--- truck/zhgy/web/js/module/products.js
+++ truck/zhgy/web/js/module/products.js
@@ -1,18 +1,18 @@
 var leaves = {};
 
-$(document).ready(function (){
+jQuery(document).ready(function (){
 	jQuery('.category-li a').each(function (){
 		jQuery(this).click(function (obj){
 			var id = $(this).parent().attr('id').substr(9);
-			$('#products_showbox').hide();
+			jQuery('#products_showbox').hide();
 			event.preventDefault();
 			event.stopPropagation();
 			jQuery('#products_showbox').load('/zhgy/ProductController.do?action=getProdcutsByCategory&id='+id,function(){
-				 $('#products_showbox').fadeIn('fast');
+				jQuery('#products_showbox').fadeIn('fast');
 			});
 		});
 	});
-	$("ul.sf-menu").superfish({ 
+	jQuery("ul.sf-menu").superfish({ 
         animation: 	{height:'show'},
         delay:		500,
         speed:		'fast',
